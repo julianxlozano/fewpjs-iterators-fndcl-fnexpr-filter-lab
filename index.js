3daf77554852d8c16855e0d803ec856c6cd2c23e
julianxlozano <julianxlozano@gmail.com>
2021-05-26 14:48:09 -0500

    commit tree 70a628b9fe1e3efff2b303c060a3d8a260ce0da1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers,string){
+    return drivers.filter(driver => {return driver === string || driver === string.toLowerCase()})
+};
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter(driver => {return driver.match("^"+string) })
+};
+
+function matchName(drivers,string){
+    return drivers.filter(driver => {return driver.name === string})  
+};
